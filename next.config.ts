4ade8e05ff9675bef6ed492d16b5d1bee74fe7ba
jagashira <pyprogjas.3104@gmail.com>
2025-05-02 23:06:46 +0900

    deploy確認

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,15 @@ const nextConfig: NextConfig = {
       "fluent-ffmpeg",
       "@ffmpeg-installer/ffmpeg",
     ],
+    esmExternals: "loose",
   },
+  webpack: (config) => {
+    // 'canvas'と'encoding'モジュールのエイリアスを無効化
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+  transpilePackages: ["@react-pdf/renderer"],
 };
 /* config options here */
 
